c9148824ca53e465d61a90ddc4139fb631b492d8
Guillermo <guille@example.com>
2024-02-22 12:32:52 +0100

    probar hacer custom queries dinamicas

---
+++ src/main/java/org/iesvdm/actividad4_5/repository/CategoriaCustomRepositoryImpl.java
@@ -0,0 +1,38 @@
+package org.iesvdm.actividad4_5.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import org.iesvdm.actividad4_5.domain.Categoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CategoriaCustomRepositoryImpl implements ICustomQuery {
+
+    @Autowired
+    private EntityManager em;
+
+    // Implementacion con JQPL
+    @Override
+    public List<Categoria> buscarCategoriaOrdenada(Optional<String> buscar, Optional<String> ordenar) {
+        StringBuilder queryBuilder = new StringBuilder("SELECT c FROM Categoria c");
+        if (buscar.isPresent()) {
+            queryBuilder.append(" WHERE c.nombre LIKE :nombre");
+        }
+        if (ordenar.isPresent()) {
+            if (buscar.isPresent() && ordenar.get().equals("asc")) {
+                queryBuilder.append(" ORDER BY c.nombre ASC");
+            } else if (buscar.isPresent() && ordenar.get().equals("desc")) {
+                queryBuilder.append(" ORDER BY c.nombre DESC");
+            }
+        }
+        Query query = em.createQuery(queryBuilder.toString());
+        if (buscar.isPresent()) {
+            query.setParameter("nombre", "%" + buscar.get() + "%");
+        }
+        return query.getResultList();
+    }
+}
---
+++ src/main/java/org/iesvdm/actividad4_5/repository/ICustomQuery.java
@@ -0,0 +1,11 @@
+package org.iesvdm.actividad4_5.repository;
+
+import org.iesvdm.actividad4_5.domain.Categoria;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ICustomQuery {
+
+    public List<Categoria> buscarCategoriaOrdenada(Optional<String> buscar, Optional<String> ordenar);
+}
