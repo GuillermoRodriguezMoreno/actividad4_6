7cecc4cd15494dc22971eafaa916f4b6787ae3a2
Guillermo <guille@example.com>
2024-02-22 12:13:44 +0100

    probar hacer custom queries dinamicas

--- src/main/java/org/iesvdm/actividad4_5/repository/CategoriaRepository.java
+++ src/main/java/org/iesvdm/actividad4_5/repository/CategoriaRepository.java
@@ -4,9 +4,11 @@ import org.iesvdm.actividad4_5.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
     // Conteo de peliculas por categoria
